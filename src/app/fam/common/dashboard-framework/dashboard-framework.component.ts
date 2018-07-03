8b4a249f4ce32ef22afc4b83e726d550c2541918
Pradipkumar Desai <pradipkumardesai@live.in>
2018-07-03 21:49:43 +0530

    module structure

---
+++ src/app/fam/common/dashboard-framework/dashboard-framework.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cch-am-dashboard-framework',
+  templateUrl: './dashboard-framework.component.html',
+  styleUrls: ['./dashboard-framework.component.scss']
+})
+export class DashboardFrameworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/fam/common/dashboard-framework/dashboard-framework.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardFrameworkComponent } from './dashboard-framework.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DashboardFrameworkComponent]
+})
+export class DashboardFrameworkModule { }
